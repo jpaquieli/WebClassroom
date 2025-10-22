72cb9eb7cbcfb35ab45d46b65c0b9d3f01241538
Julio Paquieli <juliopaquieli@MacBook-Air-de-Julio.local>
2025-10-22 20:54:44 -0300

    Adicionando rotas de findAll para usuários

--- src/http/controllers/user/routes.ts
+++ src/http/controllers/user/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { create } from './create';
 import { signin } from './signIn';
+import { findAllProfessor } from './findAllProfessor';
+import { findAllAluno } from './findAllAluno';
 
 const userRouter = Router();
 
 userRouter.post('/user', create);
 userRouter.post('/user/signin', signin);
+userRouter.get('/user/professor', findAllProfessor);
+userRouter.get('/user/aluno', findAllAluno);
 
 export default userRouter;
